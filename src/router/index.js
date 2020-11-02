14e424f3d0fcbbc29e0efd09befb22df8522bf63
wangjia <jin_wangjia@163.com>
2020-11-03 02:19:18 +0800

    页面导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -11,9 +11,14 @@ const routes = [
     component: Login
   },
   {
+    path: '/Login',
+    name: 'Login',
+    component: Login
+  },
+  {
     path: "/Home",
     name: "Home",
-    component: () => import("../views/Home")
+    component: () => import("../views/Home.vue")
   }
 
   // {
@@ -30,6 +35,24 @@ const routes = [
 const router = new VueRouter({
   mode:"hash",
   routes
+})
+
+router.beforeEach((to, from, next)=> {
+  console.log(to)
+  console.log(from)
+  //
+  if(to.path==="/Login")
+    return next()
+
+  if(to.path==="/")
+    return next()
+
+  let token = window.sessionStorage.getItem("token")
+  console.log(token)
+  if(!token){
+    next("/Login")
+  }
+  next()
 })
 
 export default router
