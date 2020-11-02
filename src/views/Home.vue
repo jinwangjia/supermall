31260bef9112b98d3d7833ac8c3a3f4263f8d0fd
wangjia <jin_wangjia@163.com>
2020-11-03 03:54:30 +0800

    登出

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,19 @@
 <template>
 <div>
   这里是home
+  <el-button type="primary" @click="logOut">buttonCont</el-button>
 </div>
 </template>
 
 <script>
 export default {
-name: "Home"
+name: "Home",
+  methods: {
+  logOut() {
+    window.sessionStorage.clear()
+    this.$router.push("/Login")
+  }
+  }
 }
 </script>
 
