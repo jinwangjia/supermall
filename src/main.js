64b214fde748e82d2d08090fd62797569d489dec
wangjia <jin_wangjia@163.com>
2020-11-02 23:03:49 +0800

    添加element-ui控件

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import {Form,FormItem,Input} from 'element-ui'
+Vue.use(Form)
+Vue.use(FormItem)
+Vue.use(Input)
+
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <el-form ref="form" label-width="80px">
+      <el-form-item label="活动名称">
+        <el-input ></el-input>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
