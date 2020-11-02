10cf37faf76d33024464e58d70bed4bb2d115f10
wangjia <jin_wangjia@163.com>
2020-11-03 04:16:58 +0800

    代码格式化 没生效

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Login from "@/views/Login";
+import Login from '@/views/Login'
 
 Vue.use(VueRouter)
 
@@ -16,41 +16,31 @@ const routes = [
     component: Login
   },
   {
-    path: "/Home",
-    name: "Home",
-    component: () => import("../views/Home.vue")
+    path: '/Home',
+    name: 'Home',
+    component: () => import('../views/Home.vue')
   }
-
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
-
 ]
 
 const router = new VueRouter({
-  mode:"hash",
+  mode: 'hash',
   routes
 })
 
-router.beforeEach((to, from, next)=> {
+router.beforeEach((to, from, next) => {
   console.log(to)
   console.log(from)
   //
-  if(to.path==="/Login")
+  if (to.path === '/Login')
     return next()
 
-  if(to.path==="/")
+  if (to.path === '/')
     return next()
 
-  let token = window.sessionStorage.getItem("token")
+  let token = window.sessionStorage.getItem('token')
   console.log(token)
-  if(!token){
-    next("/Login")
+  if (!token) {
+    next('/Login')
   }
   next()
 })
